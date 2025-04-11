9c5f7c09921d4dbe6cccf9b8fce4fa3fe272fd74
josephchapman <9332991+josephchapman@users.noreply.github.com>
2025-04-11 12:58:56 +0000

    use envvar for players list

--- replicator-chess/src/main.go
+++ replicator-chess/src/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"os"
 	"time"
 
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
@@ -15,17 +17,33 @@ const applicationName = "replicator-chess"
 func main() {
 	cmn.SetApplicationName(applicationName)
 
-	player := flag.String("player", "", "Pull ratings for this player into a TSDB")
 	sleepMinutes := flag.Int("nocron", 0, "Run in a loop with a sleep interval in minutes")
 	flag.Parse()
 
 	// Use a closure to pass the player variable to runTask
 	cmn.NoCron(func() {
-		runTask(player)
+		runTask()
 	}, *sleepMinutes)
 }
 
-func runTask(player *string) {
+func runTask() {
+	// Read the environment variable
+	envVar := os.Getenv("PLAYERS")
+	if envVar == "" {
+		err := fmt.Errorf("PLAYERS environment variable is not set")
+		cmn.LoggedError(err)
+		panic(err)
+	}
+
+	// Unmarshal the JSON array from the environment variable
+	var players []string
+	err := json.Unmarshal([]byte(envVar), &players)
+	if err != nil {
+		err = fmt.Errorf("json.Unmarshal: %w", err)
+		cmn.LoggedError(err)
+		panic(err)
+	}
+
 	// create writeAPI for influx
 	bucket := "my-bucket"
 	org := "my-org"
@@ -78,67 +96,69 @@ func runTask(player *string) {
 	// User blocking write client for writes to desired bucket
 	writeAPI := client.WriteAPIBlocking(org, bucket)
 
-	al, _ := NewArchiveList(*player)
-	for _, archiveStr := range al.Archives {
-		player, year, month, err := playerYearMonth(archiveStr)
-		if err != nil {
-			err = fmt.Errorf("playerYearMonth(): %w", err)
-			cmn.LoggedError(err)
-		} else {
-			log := fmt.Sprintf("Archive found %s-%s-%s", player, year, month)
-			cmn.LogOut.Info(log)
-		}
-
-		// Check if record[player][year][month] exists
-		if record[player] != nil && record[player][year] != nil && contains(record[player][year], month) {
-			log := fmt.Sprintf("Skipping %s-%s-%s, already in database", player, year, month)
-			cmn.LogOut.Info(log)
-		} else {
-			log := fmt.Sprintf("Adding %s-%s-%s to database", player, year, month)
-			cmn.LogOut.Info(log)
-
-			// Create a new archive object
-			archive, err := NewArchive(archiveStr)
+	// Iterate through the slice and process each leaderboard
+	for _, player := range players {
+		al, _ := NewArchiveList(player)
+		for _, archiveStr := range al.Archives {
+			player, year, month, err := playerYearMonth(archiveStr)
 			if err != nil {
-				err = fmt.Errorf("NewArchive(): %w", err)
+				err = fmt.Errorf("playerYearMonth(): %w", err)
 				cmn.LoggedError(err)
 			} else {
-				log := fmt.Sprintf("Archive created %s-%s-%s", player, year, month)
+				log := fmt.Sprintf("Archive found %s-%s-%s", player, year, month)
 				cmn.LogOut.Info(log)
 			}
 
-			// Write the TSDB record
-			entry, _ := archive.eloTsdb()
-			err = writeAPI.WriteRecord(context.Background(), entry)
-			if err != nil {
-				err = fmt.Errorf("writeAPI.WriteRecord(): %w", err)
-				cmn.LoggedError(err)
-			} else {
-				log := fmt.Sprintf("Query written: %s", archiveStr)
+			// Check if record[player][year][month] exists
+			if record[player] != nil && record[player][year] != nil && contains(record[player][year], month) {
+				log := fmt.Sprintf("Skipping %s-%s-%s, already in database", player, year, month)
 				cmn.LogOut.Info(log)
-			}
+			} else {
+				log := fmt.Sprintf("Adding %s-%s-%s to database", player, year, month)
+				cmn.LogOut.Info(log)
 
-			// if the archive is not currently being modified (it's complete), record the pull
-			if archiveStr != al.current() {
-				p := influxdb2.NewPoint("pull",
-					map[string]string{"player": player, "archive_year": year, "archive_month": month},
-					map[string]interface{}{"pulled": 1},
-					time.Now())
+				// Create a new archive object
+				archive, err := NewArchive(archiveStr)
+				if err != nil {
+					err = fmt.Errorf("NewArchive(): %w", err)
+					cmn.LoggedError(err)
+				} else {
+					log := fmt.Sprintf("Archive created %s-%s-%s", player, year, month)
+					cmn.LogOut.Info(log)
+				}
 
-				// Write point immediately
-				err = writeAPI.WritePoint(context.Background(), p)
+				// Write the TSDB record
+				entry, _ := archive.eloTsdb()
+				err = writeAPI.WriteRecord(context.Background(), entry)
 				if err != nil {
 					err = fmt.Errorf("writeAPI.WriteRecord(): %w", err)
 					cmn.LoggedError(err)
 				} else {
-					log := fmt.Sprintf("Archive complete. Pull recorded %s-%s-%s", player, year, month)
+					log := fmt.Sprintf("Query written: %s", archiveStr)
 					cmn.LogOut.Info(log)
 				}
-			} else {
-				log := fmt.Sprintf("Archive incomplete. Pull not recorded %s-%s-%s", player, year, month)
-				cmn.LogOut.Info(log)
-			}
 
+				// if the archive is not currently being modified (it's complete), record the pull
+				if archiveStr != al.current() {
+					p := influxdb2.NewPoint("pull",
+						map[string]string{"player": player, "archive_year": year, "archive_month": month},
+						map[string]interface{}{"pulled": 1},
+						time.Now())
+
+					// Write point immediately
+					err = writeAPI.WritePoint(context.Background(), p)
+					if err != nil {
+						err = fmt.Errorf("writeAPI.WriteRecord(): %w", err)
+						cmn.LoggedError(err)
+					} else {
+						log := fmt.Sprintf("Archive complete. Pull recorded %s-%s-%s", player, year, month)
+						cmn.LogOut.Info(log)
+					}
+				} else {
+					log := fmt.Sprintf("Archive incomplete. Pull not recorded %s-%s-%s", player, year, month)
+					cmn.LogOut.Info(log)
+				}
+			}
 		}
 	}
 
